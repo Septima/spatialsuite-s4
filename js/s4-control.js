83dde7c46ab022a1f27bc94dfd760385d1165fcd
klavspc <klavs@septima.dk>
2013-06-18 19:25:54 +0200

    Første version af selectkontrol

--- js/s4-control.js
+++ js/s4-control.js
@@ -4,21 +4,22 @@ Septima.Search.S4Control = Septima.Class ({
     initialize: function (options) {
     	this.options = options;
 //        {div:'',
-//        callbackHandler: object,
+//    	  placeHolder:''
+//        onSelect: function,
 //        municipality: '101',
 //        view:{limit: 20},
-//        adresssearcher:{enabled: false, info: true, apiKey: "FCF3FC50-C9F6-4D89-9D7E-6E3706C1A0BD"},
-//        geosearcher:{enabled: true, info: true, targets: ['*']},
+//        adresssearcher:{enabled: false, apiKey: "FCF3FC50-C9F6-4D89-9D7E-6E3706C1A0BD"},
+//        geosearcher:{enabled: true, targets: ['*']},
 //        // Full set of geosearcher targets is targets: ['adresser','stednavne', 'kommuner', 'matrikelnumre', 'opstillingskredse', 'politikredse', 'postdistrikter', 'regioner', 'retskredse']
-//        cvrsearcher:{enabled: true, info: true},
-//        plansearcher:{enabled: true, info: true},
-//        indexsearcher:{enabled: true, info: true, datasources: "*"}};
+//        cvrsearcher:{enabled: true},
+//        plansearcher:{enabled: true},
+//        indexsearcher:{enabled: true, datasources: "*"}};
     	
     	var searchers = [];
         
         //Set up adress searcher
         if (this.options.adresssearcher.enabled){
-        	var adressSearchOptions = {apiKey: this.options.adresssearcher.apiKey, onSelect: callbackHandler.s4Hit};
+        	var adressSearchOptions = {apiKey: this.options.adresssearcher.apiKey, onSelect: this.options.onSelect};
         	if (this.options.municipality != "*"){
         		adressSearchOptions.area = "muncode0" + this.options.municipality;
         	}
@@ -32,7 +33,7 @@ Septima.Search.S4Control = Septima.Class ({
             	var geoSearchOptions = {
             			targets: this.options.geosearcher.targets,
             			authParams: {ticket: kmsTicket},
-            		    onSelect: callbackHandler.s4Hit
+            		    onSelect: this.options.onSelect
             	    };
             	if (this.options.municipality != "*"){
             		geoSearchOptions.area = "muncode0" + this.options.municipality;
@@ -43,7 +44,7 @@ Septima.Search.S4Control = Septima.Class ({
         }
         
         if (this.options.cvrsearcher.enabled){
-        	var cvr_enhedSearchOptions = {onSelect: callbackHandler.s4Hit};
+        	var cvr_enhedSearchOptions = {onSelect: this.options.onSelect};
         	var se = new Septima.Search.CVR_enhedSearcher(cvr_enhedSearchOptions);
         	if (this.options.municipality != "*"){
             	se.filter = { 'kom_id_officiel' : this.options.municipality };
@@ -52,7 +53,7 @@ Septima.Search.S4Control = Septima.Class ({
         }
 
         if (this.options.plansearcher.enabled){
-        	var planSearchOptions = {onSelect: callbackHandler.s4Hit};
+        	var planSearchOptions = {onSelect: this.options.onSelect};
         	var planSearcher = new Septima.Search.PlanSearcher(planSearchOptions);
         	if (this.options.municipality != "*"){
         		planSearcher.filter = { 'komnr' : this.options.municipality };
@@ -61,17 +62,25 @@ Septima.Search.S4Control = Septima.Class ({
         }
     	
         if (this.options.indexsearcher.enabled){
-        	var s4IndexSearcherOptions = {indexProtocol: location.protocol, indexHost: location.hostname, indexPort: location.port, onSelect: callbackHandler.s4Hit, datasources: this.options.indexsearcher.datasources};
+        	var s4IndexSearcherOptions = {indexProtocol: location.protocol, indexHost: location.hostname, indexPort: location.port, onSelect: this.options.onSelect, datasources: this.options.indexsearcher.datasources};
         	var s4IndexSearcher = new Septima.Search.S4IndexSearcher(s4IndexSearcherOptions);
         	searchers.push({title: "Objekter", searcher: s4IndexSearcher});
         }
     	
-    	this.view = new Septima.Search.DefaultView({input:this.options.div, placeholder:inputPlaceHolder, limit: this.options.view.limit});
+    	this.view = new Septima.Search.DefaultView({input:this.options.div, placeholder:this.options.placeHolder, limit: this.options.view.limit});
     	
     	var controllerOptions = {};
     	var controller = new Septima.Search.Controller(searchers, this.view, controllerOptions);
     	controller.go ();
     	
+    },
+    
+    showFixedText: function (text, hideCallback){
+    	this.view.showFixedText(text, hideCallback);
+    },
+    
+    clear: function(){
+    	this.view.doTextSearch("");
     },
 	
 	
